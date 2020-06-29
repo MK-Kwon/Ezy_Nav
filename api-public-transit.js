a24b37d0c0ba77deb108086975fe091c560e1f28
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-06-29 23:32:27 +0930

    Added function to add 'manuevers' to panel

--- api-public-transit.js
+++ api-public-transit.js
@@ -183,3 +183,33 @@ function convertToImperial(distance){
     let feet = Math.trunc(remainder / 3.28084);
     return (miles + " miles and" + feet + " feet.");
 }
+
+function addManueversToPanel(route){
+    let nodeOL = document.createElement('ol'),
+        i,
+        j;
+    nodeOL.style.fontSize = 'small';
+    nodeOL.style.marginLeft = '5%';
+    nodeOL.style.marginRight = '5%';
+    nodeOL.className = 'directions';
+
+    // Add a marker for each maneuver
+    for (i = 0; i < route.leg.length; i++) {
+        for (j = 0; j < route.leg[i].maneuver.length; j++) {
+            // Get the next manueuver
+            maneuver = route.leg[i].maneuver[j];
+           
+            let li = document.createElement('li'),
+                spanArrow = document.createElement('span'),
+                spanInstruction = document.createElement('span');
+                
+            spanArrow.className = 'arrow ' + maneuver.action;
+            spanInstruction.innerHTML = maneuver.instruction;
+            li.appendChild(spanArrow);
+            li.appendChild(spanInstruction);
+            
+            nodeOL.appendChild(li);
+        }
+    }
+    routeInstructionsContainer.appendChild(nodeOL);
+}
